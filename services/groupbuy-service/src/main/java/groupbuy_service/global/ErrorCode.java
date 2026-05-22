ec41ab25ba659f7c399dd340864b020519c9e48b
one-zeze <tales0708@gmail.com>
2026-05-22 17:30:37 +0900

    feat(groupbuy-service): 전역 예외 처리(Global Exception Handling) 구현 및 엔티티 검증 로직 추가
    - CustomExceptionHandler 추가 (@RestControllerAdvice)
    - BusinessException 및 ErrorCode(Enum), ErrorResponse DTO 추가
    - Groupbuy, Participation, Product 엔티티에 BusinessException 기반의 Fail-Fast 검증 로직(수량, 가격, 기간 등) 적용

---
+++ services/groupbuy-service/src/main/java/groupbuy_service/global/ErrorCode.java
@@ -0,0 +1,31 @@
+package groupbuy_service.global;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    //server
+    INTERNAL_ERROR(500,"서버에러"),
+
+    //user
+    USER_NOT_FOUND(404,"사용자를 찾을 수 없습니다"),
+    EMAIL_ALREADY_EXISTS(400,"동일한 이메일의 사용자가 이미 존재합니다"),
+
+    //product, groupbuy, participation
+    PRODUCT_NAME_ALREADY_EXISTS(400,"동일한 이름의 상품이 이미 존재합니다"),
+    PRODUCT_NOT_FOUND(404,"상품을 찾을수 없습니다"),
+    PRODUCT_OUT_OF_STOCK(400,"재고가 없습니다."),
+    PARTICIPATION_DATE_EXPIRED(400,"참여 기간이 아닙니다"),
+    ORDER_QUANTITY_INVALID(400,"주문 수량이 올바르지 않습니다"),
+    GROUPBUY_STARTAT_INVALID(400,"시작일이 올바르지 않습니다"),
+    GROUPBUY_ENDAT_INVALID(400,"종료일이 올바르지 않습니다"),
+    INITIAL_STOCK_INVALID(400, "초기 재고는 0보다 작을 수 없습니다."),
+    PRODUCT_PRICE_INVALID(400, "상품 가격은 0보다 작을 수 없습니다."),
+    TARGET_QUANTITY_INVALID(400, "목표 수량이 올바르지 않습니다."),
+    ;
+
+    private final int status;
+    private final String msg;
+}
--- services/groupbuy-service/src/main/java/groupbuy_service/participation/domain/Participation.java
+++ services/groupbuy-service/src/main/java/groupbuy_service/participation/domain/Participation.java
@@ -1,6 +1,8 @@
 package groupbuy_service.participation.domain;
 
 import groupbuy_service.common.BaseEntity;
+import groupbuy_service.global.BusinessException;
+import groupbuy_service.global.ErrorCode;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,12 +27,26 @@ public class Participation extends BaseEntity {
   private ParticipationStatus status;
 
   @Builder
-  public Participation(String groupbuyId, String productId, String userId, int quantity){
+  public Participation(String groupbuyId, String productId, String userId, int quantity) {
+    validateQuantity(quantity);
+
     this.participationId = UUID.randomUUID().toString();
     this.groupbuyId = groupbuyId;
     this. productId = productId;
     this. userId = userId;
     this.quantity = quantity;
+    this.status = ParticipationStatus.REQUESTED;
   }
+
+  public void updateStatus(ParticipationStatus status) {
+    this.status = status;
+  }
+
+  private void validateQuantity(int quantity) {
+    if (quantity < 1) {
+      throw new BusinessException(ErrorCode.ORDER_QUANTITY_INVALID);
+    }
+  }
+
 
 }
