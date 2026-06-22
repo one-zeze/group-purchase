e95c538992c9d1391e261c6055a5bfa313f6ccf5
one-zeze <tales0708@gmail.com>
2026-06-22 18:02:11 +0900

    feat(groupbuy-service): 주문 및 결제 도메인 엔티티, 스키마 추가

---
+++ services/groupbuy-service/src/main/java/groupbuy_service/order/domain/Order.java
@@ -0,0 +1,51 @@
+package groupbuy_service.order.domain;
+
+import groupbuy_service.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_order")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseEntity {
+
+    @Id
+    private String orderId;
+
+    private String groupbuyId;
+    private String productId;
+    private String userId;
+    private String participationId;
+
+    private int quantity;
+    private BigDecimal totalPrice;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Builder
+    public Order (String groupbuyId,
+                  String productId,
+                  String userId,
+                  String participationId,
+                  int quantity,
+                  BigDecimal totalPrice
+    ) {
+        this.orderId = UUID.randomUUID().toString();
+        this.status = OrderStatus.CREATED;
+
+        this.groupbuyId = groupbuyId;
+        this.productId = productId;
+        this.userId = userId;
+        this.participationId = participationId;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+}
